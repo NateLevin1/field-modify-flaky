959964448c49b22381820e3f2850f634fa1623c5
Nate Levin <63370104+NateLevin1@users.noreply.github.com>
2023-10-28 21:34:04 -0400

    Update NumbersModifierTest.java

--- src/test/java/in/natelev/NumbersModifierTest.java
+++ src/test/java/in/natelev/NumbersModifierTest.java
@@ -3,9 +3,17 @@ package in.natelev;
 import org.junit.Test;
 
 public class NumbersModifierTest {
+    // polluter
     @Test
     public void testAdd() {
         NumbersModifier numbersModifier = new NumbersModifier();
-        numbersModifier.add(10);
+        numbersModifier.add(10), 10);
+    }
+    
+    // cleaner
+    @Test
+    public void testReset() {
+        NumbersModifier numbersModifier = new NumbersModifier();
+        numbersModifier.reset();
     }
 }
