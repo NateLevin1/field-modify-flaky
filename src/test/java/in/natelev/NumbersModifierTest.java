7d40a67ef0003212d842dcab9549aac839c7ed24
Nate Levin <63370104+NateLevin1@users.noreply.github.com>
2023-10-28 21:38:04 -0400

    Fix trivial NumbersModifierTest syntax error

--- src/test/java/in/natelev/NumbersModifierTest.java
+++ src/test/java/in/natelev/NumbersModifierTest.java
@@ -7,7 +7,7 @@ public class NumbersModifierTest {
     @Test
     public void testAdd() {
         NumbersModifier numbersModifier = new NumbersModifier();
-        numbersModifier.add(10), 10);
+        numbersModifier.add(10);
     }
     
     // cleaner
